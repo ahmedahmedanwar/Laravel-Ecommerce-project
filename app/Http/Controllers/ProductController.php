62b7f508356e7954000a5e3b2900a150f30906ec
AhmedAnwar <ahmed.mido20101234@gmail.com>
2021-01-27 13:21:31 +0200

    after login work

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Prouduct;
 class ProductController extends Controller
 {
     //
     function index(){
-        return "welcom To main page";
+        $data = Prouduct::all();
+        return view('product',['product'=>$data]);
     }
 }
